6247624b5392d8c50942a2e382acf5369941016c
inabajunmr <inabajun.for.regi@gmail.com>
2019-01-11 02:02:31 +0900

    set ng button

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -7,7 +7,30 @@ chrome.extension.sendMessage({}, function(response) {
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
 		// ----------------------------------------------------------
-
+		setNgButton()
 	}
 	}, 10);
 });
+
+function setNgButton(){
+
+	tds = document.querySelectorAll(".mp_tbl1 tr>td:nth-child(4)")
+
+	tds.forEach((td) => {
+		name = td.textContent
+		var button = document.createElement("input");
+		button.setAttribute("value", "NG");
+		button.setAttribute("type", "button");
+		button.setAttribute("class", "ng");
+		button.setAttribute("name", name);
+		
+		button.addEventListener('click', function() {
+  
+			console.log('heys');
+		  
+		}, false);
+		
+
+		td.appendChild(button);
+	});
+}
